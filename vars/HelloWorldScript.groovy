60724aa1329ef86c7c5758d3f23325ad83559242
stefancosic93 <90192297+stefancosic93@users.noreply.github.com>
2021-09-08 10:06:02 +0200

    Update HelloWorldScript.groovy

--- vars/HelloWorldScript.groovy
+++ vars/HelloWorldScript.groovy
@@ -1,11 +1,3 @@
-println("Hello, Groovy!")
-call()
-
-def call() {
-    println('Function CALL was initiated!')
-}
-
 def callName(String name = 'human') {
     echo "Hello, ${name}."
-    call()
 }
