ccf64a027584cec96e335e8c63c02635efca8b55
stefancosic93 <90192297+stefancosic93@users.noreply.github.com>
2021-09-08 12:17:33 +0200

    Create GlobalVars.groovy

---
+++ src/pkgName/GlobalVars.groovy
@@ -0,0 +1,10 @@
+package pkgName
+
+class GlobalVars {
+   static String foo = "bar"
+
+   // refer to this in a pipeline using:
+   //
+   // import pkgName.GlobalVars
+   // println GlobalVars.foo
+}
